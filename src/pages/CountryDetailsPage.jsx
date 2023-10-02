3eb6121d6e33b14b62aaafee91aa7aa467d41c9d
Marina Muñoz <marinacerejidoo@gmail.com>
2023-10-02 16:59:56 +0200

    Finished lab without CSS

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,25 @@
-function CountryDetails() {}
+import { useParams } from "react-router-dom";
+
+function CountryDetails(props) {
+  //have to delete it
+  const { alpha3Code } = useParams();
+
+  const selectedCountry = props.send?.filter(
+    (country) => country.alpha3Code === alpha3Code
+  );
+  return (
+    <div>
+      <h1>Country Details</h1>
+      {selectedCountry.map((currentCountry) => (
+        <div>
+          <h2>Country:{currentCountry.name.common}</h2>
+          <h3>Capital:{currentCountry.capital}</h3>
+          <h3>Area: {currentCountry.area}</h3>
+          <h3>Borders: {currentCountry.borders}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,27 @@
-function HomePage() {}
+import { Link } from "react-router-dom";
+
+function HomePage(props) {
+  return props.send ? (
+    <div>
+      <h1>WikiCountries: Your Guide to the World</h1>
+
+      {props.send.map((country) => {
+        return (
+          <div key={country._id}>
+            <Link to={`/${country.alpha3Code}`}>{country.name.common}</Link>
+            <img
+              src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+              style={{ width: "40px" }}
+            />
+          </div>
+        );
+      })}
+    </div>
+  ) : (
+    <>
+      <h1>Loading...</h1>
+    </>
+  );
+}
 
 export default HomePage;
